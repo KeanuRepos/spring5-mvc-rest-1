1520fddbb8c077f1291534507e29a30f8cfdcc20
kmjINTL <johnstonkeanu3@gmail.com>
2021-02-26 15:32:40 +0200

    adding ref to mapstruct

--- src/test/java/guru/springfamework/controllers/v1/CustomerControllerTest.java
+++ src/test/java/guru/springfamework/controllers/v1/CustomerControllerTest.java
@@ -25,6 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//fix1
 public class CustomerControllerTest extends AbstractRestControllerTest {
 
     @Mock
